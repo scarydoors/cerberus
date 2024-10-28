8f5f49eb9f070d42d4b2324dca227748167d573d
Alan Strauhs <alan.strauhs@pagabogroup.com>
2024-10-28 14:37:33 +0000

    refactor: remove redundant layout classes

--- src/routes/onboarding/+page.svelte
+++ src/routes/onboarding/+page.svelte
@@ -12,10 +12,9 @@ let shouldUseDefaultLocation: boolean = $state(true);
 
 </script>
 
-<div class="flex h-full flex-col py-12 px-10 bg-stone-100 overflow-x-hidden items-center w-full">
-  <h1 class="text-3xl font-semibold mb-8 text-center">Select a location for your vaults.</h1>
-  <div class="flex flex-col max-w-3xl w-full justify-center">
-    <div class="space-y-4 w-full">
+<div class="flex h-full flex-col pt-12 pb-6 px-10 bg-stone-100 overflow-x-hidden items-center w-full justify-between">
+  <h1 class="text-3xl font-semibold text-center">Select a location for your vaults.</h1>
+  <div class="flex flex-col max-w-xl w-full space-y-2 py-4">
       <div class="flex items-center space-x-2">
         <input type="checkbox" id="defaultLocation" bind:checked={shouldUseDefaultLocation} />
         <label for="defaultLocation">Use the default location for my vaults</label>
@@ -25,6 +24,13 @@ let shouldUseDefaultLocation: boolean = $state(true);
 
         <button class="bg-blue-500 text-white py-2 px-4 rounded-lg text-nowrap" onclick={openFile} disabled={shouldUseDefaultLocation}>Browse Files</button>
       </div>
-    </div>
+  </div>
+  <div class="flex space-x-1 w-full">
+    <button class="flex-1 bg-blue-500 text-white py-2 px-4">
+      Back
+    </button>
+    <button class="flex-1 bg-blue-500 text-white py-2 px-4">
+      Next
+    </button>
   </div>
 </div>
