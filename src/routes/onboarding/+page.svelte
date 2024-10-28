817a0211ec474867e0621a097685b77ea150cd74
Alan Strauhs <alan.strauhs@pagabogroup.com>
2024-10-28 16:10:27 +0000

    feat: master password step

--- src/routes/onboarding/+page.svelte
+++ src/routes/onboarding/+page.svelte
@@ -1,36 +1,62 @@
 <script lang="ts">
-import { open } from '@tauri-apps/plugin-dialog';
+  import OnboardingEnterMasterPassword from "./OnboardingEnterMasterPassword.svelte";
+import OnboardingSelectDirectory from "./OnboardingSelectDirectory.svelte";
 
-async function openFile() {
-  const file = await open({
-    multiple: false,
-    directory: true,
-  });
-}
+  const steps = [
+    'select-directory',
+    'enter-master-password',
+  ];
 
-let shouldUseDefaultLocation: boolean = $state(true);
+  type Steps = typeof steps;
+  type Step = Steps[number];
+
+  let isStepValid: boolean = $state(true);
+
+  let vaultDirectory: string = $state('~/vaults');
+  let masterPassword: string = $state('');
+
+  let activeStepIndex: number = $state(0);
+  let activeStep: Step = $derived(steps[activeStepIndex]);
+
+  let hasPreviousStep: boolean = $derived(activeStepIndex - 1 >= 0);
+  let hasNextStep: boolean = $derived(activeStepIndex + 1 < steps.length);
+
+  let showFinishButton: boolean = $derived(activeStepIndex === steps.length - 1 && isStepValid);
+
+  function previousStep() {
+    activeStepIndex = Math.max(activeStepIndex - 1, 0);
+  };
+
+  function nextStep() {
+    activeStepIndex = Math.min(activeStepIndex + 1, steps.length - 1);
+  }
 
 </script>
 
 <div class="flex h-full flex-col pt-12 pb-6 px-10 bg-stone-100 overflow-x-hidden items-center w-full justify-between">
-  <h1 class="text-3xl font-semibold text-center">Select a location for your vaults.</h1>
-  <div class="flex flex-col max-w-xl w-full space-y-2 py-4">
-      <div class="flex items-center space-x-2">
-        <input type="checkbox" id="defaultLocation" bind:checked={shouldUseDefaultLocation} />
-        <label for="defaultLocation">Use the default location for my vaults</label>
-      </div>
-      <div class="flex space-x-2">
-        <input class="flex-1 min-w-0" disabled={shouldUseDefaultLocation}/>
-
-        <button class="bg-blue-500 text-white py-2 px-4 rounded-lg text-nowrap" onclick={openFile} disabled={shouldUseDefaultLocation}>Browse Files</button>
-      </div>
-  </div>
+  {#if activeStep === 'select-directory'}
+    <OnboardingSelectDirectory bind:directory={vaultDirectory} bind:valid={isStepValid} />
+  {:else if activeStep === 'enter-master-password'}
+    <OnboardingEnterMasterPassword bind:password={masterPassword} bind:valid={isStepValid} />
+  {/if}
   <div class="flex space-x-1 w-full">
-    <button class="flex-1 bg-blue-500 text-white py-2 px-4">
-      Back
-    </button>
-    <button class="flex-1 bg-blue-500 text-white py-2 px-4">
-      Next
-    </button>
+    <div class="w-1/2">
+    {#if hasPreviousStep}
+      <button class="bg-blue-500 text-white py-2 px-4 w-full" onclick={previousStep}>
+        Back
+      </button>
+    {/if}
+      </div>
+    <div class="w-1/2">
+      {#if showFinishButton}
+        <button class="bg-blue-500 text-white py-2 px-4 w-full">
+          Finish
+        </button>
+      {:else if hasNextStep}
+        <button class="bg-blue-500 text-white py-2 px-4 w-full" disabled={!isStepValid} onclick={nextStep}>
+          Next
+        </button>
+      {/if}
+      </div>
   </div>
 </div>
---
+++ src/routes/onboarding/OnboardingEnterMasterPassword.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { zxcvbn } from '@zxcvbn-ts/core';
+
+type Props = {
+  password: string;
+  valid: boolean;
+};
+
+let { password = $bindable(), valid = $bindable() }: Props = $props();
+
+$effect(() => {
+  if (password.length > 0) {
+    valid = true;
+  } else {
+    valid = false;
+  }
+
+  console.log(zxcvbn(password));
+});
+
+</script>
+
+<h1 class="text-3xl font-semibold text-center">Create your master password.</h1>
+<div class="flex flex-col max-w-xl w-full space-y-2 py-4">
+  <input bind:value={password} type="password"/>
+</div>
